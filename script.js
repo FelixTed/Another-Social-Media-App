3b4e06efd0ef24094a71aa93495b8d59122f8f30
Felix Cadieux <felix.cadieux@eleve.csp.qc.ca>
2024-11-18 22:03:44 -0500

    opps

---
+++ script.js
@@ -0,0 +1,9 @@
+const user1 = {
+    following:[],
+    followers:[],
+    postHistory:[],
+    name: 'Felix',
+    profilePic: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    bio: 'Genie Log 2028 polymtl',
+    Stories: []
+}
