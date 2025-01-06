ffe159ee8aa3dd07ff0b6a0a4b150c0c6fc25072
Felix Cadieux <felix.cadieux@eleve.csp.qc.ca>
2025-01-06 18:16:11 -0500

    can select individual posts from someones profile

--- profile.js
+++ profile.js
@@ -1,5 +1,5 @@
 import { jwtDecode } from "https://esm.run/jwt-decode";
-import {getUserObjectById,updateUser, getPostObjectById} from './apiInteractions.js'
+import {getUserObjectById,updateUser, getPostObjectById,updatePost, getCommentObjectById, postComment} from './apiInteractions.js'
 
 let currentUser;
 const token = localStorage.getItem('token');
@@ -35,12 +35,159 @@ followButton.addEventListener('click', async () => {
 async function displayPostHistory(){
     const postHistoryBox = document.getElementById('post-history-box');
     for (const postId of selectedUserObj.postHistory){
-        const post = await getPostObjectById(postId)
+        const post = await getPostObjectById(postId);
         let previewHtml = `<div class="post-preview">
-                                <img src=${post.imageUrl} class="preview-image">
-                            </div>`
-        postHistoryBox.innerHTML += previewHtml
+                                <img src=${post.imageUrl} class="preview-image" data-post=${encodeURIComponent(JSON.stringify(post))}>
+                            </div>`;
+        postHistoryBox.innerHTML += previewHtml;
     }
+
+    postHistoryBox.addEventListener('click', async (event) => {
+            const target = event.target;
+    
+            // Check if a follow button was clicked
+            if (target.closest('.post-preview')) {
+                let postElement = JSON.parse(decodeURIComponent(event.target.dataset.post));
+                const selectedPostDiv = document.getElementById('selected-container');
+                selectedPostDiv.style.display = 'flex';
+                const selectedPostHtml = `<div id="cross-div">
+                                            <i class="material-icons">close</i>
+                                        </div>
+                
+                                        <div class="post">
+                                            <div class="post-profile">
+                                                <img class="profile-img" src=${selectedUserObj.imageUrl}>
+                                                <span>${selectedUserObj.name}</span>
+                                            </div>
+                                                <img class="post-content" src=${postElement.imageUrl}>
+                                                <div class="post-icons">
+                                                    <i id="like" class="material-icons" style="color: white;">favorite</i>
+                                                    <i id="comment" class="material-icons" style="color: white;">comment</i>
+                                                </div>
+                                                <span class="post-caption"> ${postElement.caption}</span>
+                                                
+                                        </div>`;
+                selectedPostDiv.innerHTML = selectedPostHtml;
+                document.getElementById('cross-div').addEventListener('click', () => {
+                    selectedPostDiv.innerHTML = '';
+                    selectedPostDiv.style.display = 'none';
+                })
+
+                const likesIcon = document.getElementById('like');
+                likesIcon.setAttribute('class','material-icons');
+                if (postElement.likedBy.includes(currentUser)){
+                    likesIcon.setAttribute('style','color:red;cursor:pointer;');
+                }
+                else{
+                    likesIcon.setAttribute('style','color:white;cursor:pointer');
+                }
+                
+
+                let isProcessing = false;
+                likesIcon.addEventListener('click', async () => {
+                    if (isProcessing) return;
+                    try{
+                        isProcessing = true;
+                        postElement = await updatePost(postElement._id,{likedBy:currentUser});
+
+                        if (postElement.likedBy.includes(currentUser)){
+                            likesIcon.setAttribute('style','color:red;cursor:pointer;');
+                            console.log(postElement.likedBy)
+                        }else{
+                            likesIcon.setAttribute('style','color:white;cursor:pointer;');
+                        }
+
+                        
+                    }catch(err){
+                        console.log('Error updating like status: '+ err);
+                    }finally{
+                        isProcessing = false;
+                    }
+                });
+
+                const commentIcon = document.getElementById('comment');
+            
+                commentIcon.addEventListener('click',async () => {
+                    await displayComments(postElement);
+                })
+            }
+        });
+}
+
+// Displays the comments 
+async function displayComments(selectedPost){
+    //Avoids an issue with the rendering of comments but affects performance
+    selectedPost = await getPostObjectById(selectedPost._id);
+    
+    const commentsContainer = document.getElementById('comments-container');
+    commentsContainer.style.display = 'flex';
+
+    const closeComments = document.createElement('i'); 
+    closeComments.setAttribute('class','material-icons');
+    closeComments.setAttribute('style','color:white; width: fit-content;cursor:pointer;');
+    closeComments.innerHTML = 'close';
+
+    closeComments.addEventListener('click', () =>{
+        commentsContainer.innerHTML = ''
+        commentsContainer.style.display = 'none';
+    });
+
+    const commentsBox = document.createElement('div');
+    commentsBox.setAttribute('id','comments-box')
+
+    commentsContainer.appendChild(closeComments);
+    commentsContainer.appendChild(commentsBox);
+    
+    for (const commentId of selectedPost.comments){
+        const comment = await getCommentObjectById(commentId);
+        const commentOwner = await getUserObjectById(comment.ownerId);
+
+        const content = 
+        `<div class = 'comment'>
+                <div class="profile" data-user-id="${commentOwner._id}">
+                    <img class="profile-img" src=${commentOwner.imageUrl}>
+                    <span>${commentOwner.name}</span>
+                </div>
+                <span>${comment.content}</span>
+            </div>
+        </div>`;
+
+        commentsBox.insertAdjacentHTML('beforeend',content);
+
+    }
+    
+    commentsContainer.addEventListener('click', async (event) => {
+            const target = event.target;
+    
+            // Check if a profile was clicked
+            if(target.closest('.profile')){
+                const userElement = target.closest('.profile');
+    
+                localStorage.setItem('selected-user',userElement.dataset.userId)
+                window.location.href = 'profile.html';
+            }
+        });
+    const commentInput = document.createElement('input');
+    commentInput.setAttribute('placeholder','Enter comment...');
+
+    // Add comment when enter is pressed
+    commentInput.addEventListener('keydown',async (event) =>{
+        if (event.key === "Enter") {
+            const newComment = {
+                ownerId: currentUser,
+                postId: selectedPost._id,
+                content: event.target.value
+            }
+
+            let commentToAdd = await postComment(newComment);
+            selectedPost =  await updatePost(selectedPost._id,{comments:commentToAdd._id});
+            commentsContainer.innerHTML = '';
+            displayComments(selectedPost);
+        }
+
+    });
+
+    commentsContainer.appendChild(commentInput);
 }
 
 async function displayCurrUser(id) {
@@ -69,6 +216,8 @@ async function displayCurrUser(id) {
     }
 
     displayPostHistory()
+
+    
 
 }
 
