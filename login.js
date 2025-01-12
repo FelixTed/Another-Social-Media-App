d7a2ee28b5362ea4eebc0698b7bcc3d560952053
Felix Cadieux <cadieuxfelix@outlook.com>
2025-01-12 15:37:22 -0500

    Updated CORS

--- login.js
+++ login.js
@@ -31,6 +31,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ name, password }),
+            credentials:'include',
         });
 
         const data = await response.json();
